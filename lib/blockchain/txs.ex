8fff44f50567bf5be821766b5202f885626d828d
Kabei <kambei.sapote@gmail.com>
2022-10-19 16:40:41 -0400

    txs total_output

--- lib/blockchain/txs.ex
+++ lib/blockchain/txs.ex
@@ -1573,6 +1573,7 @@ defmodule Ipncore.Tx do
       sig_count: x.sig_count,
       time: x.time,
       total_input: x.total_input,
+      total_output: x.total_output,
       vsn: x.vsn
     }
   end

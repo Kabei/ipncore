a200f7e3f076942c4ab8007cedd05e53ed922ca9
Kabei <kambei.sapote@gmail.com>
2022-10-19 14:56:21 -0400

    update migration

--- lib/migration.ex
+++ lib/migration.ex
@@ -7,35 +7,62 @@ defmodule Ipncore.Migration do
     System
   }
 
+  @prefix "sys"
   @otp_app :ipncore
+  @channel Application.get_env(@otp_app, :channel)
 
   def start do
     migration_version = Application.get_env(@otp_app, :migration_version)
-    # channel_version = Application.get_env(@otp_app, :channel_version)
 
-    if not Repo.schema_exists?("sys") do
+    if not Repo.schema_exists?(@prefix) do
       System.build(%{"version" => migration_version})
-    else
-      current_version =
-        from(env in "env", where: env.key == "version", select: env.value, limit: 1)
-        |> Repo.one(prefix: "sys")
-        |> String.to_integer()
+    end
 
-      if migration_version > current_version do
-        System.build(%{"version" => migration_version})
-      end
+    Channel.all()
+    |> cond do
+      [] ->
+        Channel.new(%{
+          "id" => @channel,
+          "pubkey" => PlatformOwner.pubkey(),
+          "time" => :erlang.system_time(:millisecond)
+        })
+        |> Repo.insert(prefix: @prefix)
 
-      channels = Channel.all()
+        Blockchain.build(%{"channel" => @channel, "version" => migration_version})
 
-      for channel <- channels do
-        if Repo.schema_exists?(channel.id) do
-          if migration_version > channel.vsn do
-            Blockchain.build(%{"channel" => channel.id, "version" => migration_version})
-          end
-        else
+      channels ->
+        for channel <- channels do
           Blockchain.build(%{"channel" => channel.id, "version" => migration_version})
         end
-      end
     end
   end
+
+  # def start do
+  #   migration_version = Application.get_env(@otp_app, :migration_version)
+
+  #   if not Repo.schema_exists?("sys") do
+  #     System.build(%{"version" => migration_version})
+  #   else
+  #     current_version =
+  #       from(env in "env", where: env.key == "version", select: env.value, limit: 1)
+  #       |> Repo.one(prefix: "sys")
+  #       |> String.to_integer()
+
+  #     if migration_version > current_version do
+  #       System.build(%{"version" => migration_version})
+  #     end
+
+  #     channels = Channel.all()
+
+  #     for channel <- channels do
+  #       if Repo.schema_exists?(channel.id) do
+  #         if migration_version > channel.vsn do
+  #           Blockchain.build(%{"channel" => channel.id, "version" => migration_version})
+  #         end
+  #       else
+  #         Blockchain.build(%{"channel" => channel.id, "version" => migration_version})
+  #       end
+  #     end
+  #   end
+  # end
 end

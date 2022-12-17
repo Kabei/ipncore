ba6866d5fef42723d6a7c22191189681cdf8053e
Kabei <kambei.sapote@gmail.com>
2022-12-16 22:53:08 -0400

    update

--- lib/ipncore/dns_tcp_server.ex
+++ lib/ipncore/dns_tcp_server.ex
@@ -26,6 +26,9 @@ defmodule Ipncore.DNS.TcpServer do
       response = Ipncore.DNS.handle(record, socket)
 
       :ok = :gen_tcp.send(socket, DNS.Record.encode(response))
+    catch
+      e ->
+        IO.puts("Error DNS")
     rescue
       e ->
         Logger.error(Exception.format(:error, e, __STACKTRACE__))

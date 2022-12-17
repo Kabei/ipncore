710b15328e571175b616121377426130e20fc746
Kabei <kambei.sapote@gmail.com>
2022-12-17 00:50:47 -0400

    update

--- lib/ipncore/dns_tcp_server.ex
+++ lib/ipncore/dns_tcp_server.ex
@@ -26,7 +26,7 @@ defmodule Ipncore.DNS.TcpServer do
 
   def accept(ip, port) do
     {:ok, listen_socket} =
-      :gen_tcp.listen(port, [:binary, packet: :raw, active: :once, reuseaddr: true])
+      :gen_tcp.listen(port, [:binary, packet: :raw, active: false, reuseaddr: true])
 
     IO.puts("DNS Server listening at TCP #{Inet.to_str(ip)}:#{port}")
 

6fc48921289a15f9d86f7d21b97877c60252039a
Kabei <kambei.sapote@gmail.com>
2023-03-03 04:47:55 -0400

    ufw

--- script/ufw.sh
+++ script/ufw.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
-apt-get install ufw
+apt-get install ufw -y
+ufw reset
 ufw default deny incoming
 ufw default allow outgoing
 ufw allow ssh
 ufw allow http
 ufw allow https
+#ufw allow 443/udp
 ufw allow 53/udp
 ufw allow 853/tcp
 ufw enable

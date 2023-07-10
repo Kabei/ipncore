47546814c6ca460e96bacb72579dd5c970cb96c0
kabei <kambei.sapote@gmail.com>
2023-07-10 18:39:36 -0400

    udpate srcipt run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 killall beam.smp
 
-if [ "$MODE" = "iex" ] then
+if [ "$MODE" = "iex" ]; then
     iex --name ${NODE} --cookie ${COOKIE} -S mix
-elif [ -n "$LOG" ] then
+elif [ -n "$LOG" ]; then
     elixir --name ${NODE} --cookie ${COOKIE} -S mix run --no-halt > $LOG
 else
     elixir --name ${NODE} --cookie ${COOKIE} -S mix run --no-halt

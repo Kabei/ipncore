1449d47d9aced17302202975d8f95c6c9f106f6c
kabei <kambei.sapote@gmail.com>
2023-07-13 20:59:07 -0400

    debug

--- lib/ipncore/p2p/server.ex
+++ lib/ipncore/p2p/server.ex
@@ -169,9 +169,9 @@ defmodule Ippan.P2P.Server do
 
   defp decode!(packet, sharedkey) do
     <<iv::bytes-size(@iv_bytes), tag::bytes-size(@tag_bytes), ciphertext::binary>> = packet
-    IO.inspect(iv)
-    IO.inspect(tag)
-    IO.inspect(ciphertext)
+    IO.inspect(packet, limit: :infinity)
+
+    # <<riv::bytes-size(12), rtag::bytes-size(16), rciphertext::binary>> = r
 
     :crypto.crypto_one_time_aead(
       :chacha20_poly1305,

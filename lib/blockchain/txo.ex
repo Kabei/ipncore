851e9c021286c5c23388dc5ec0ab928d81a45716
Kambei Sapote <7316758+Kabei@users.noreply.github.com>
2023-09-01 10:24:17 -0400

    Update txo.ex

--- lib/blockchain/txo.ex
+++ lib/blockchain/txo.ex
@@ -24,8 +24,8 @@ defmodule Ipncore.Txo do
   end
 
   def all(params) do
-    from(txo in Txo,
-      join: ev in Event,
+    from(ev in Event,
+      join: txo in Txo,
       on: ev.hash == txo.txid
     )
     |> filter_index(params)
@@ -43,48 +43,48 @@ defmodule Ipncore.Txo do
   defp filter_index(query, %{"txid" => txid}) do
     txid = Event.decode_id(txid)
 
-    where(query, [txo], txo.txid == ^txid)
+    where(query, [_ev, txo], txo.txid == ^txid)
   end
 
   defp filter_index(query, _params), do: query
 
   defp filter_address(query, %{"from" => address}) do
     bin_address = Address.from_text(address)
-    where(query, [txo], txo.from == ^bin_address)
+    where(query, [_ev, txo], txo.from == ^bin_address)
   end
 
   defp filter_address(query, %{"to" => address}) do
     bin_address = Address.from_text(address)
-    where(query, [txo], txo.to == ^bin_address)
+    where(query, [_ev, txo], txo.to == ^bin_address)
   end
 
   defp filter_address(query, %{"from" => from_address, "to" => to_address}) do
     to_address = Address.from_text(to_address)
     from_address = Address.from_text(from_address)
-    where(query, [txo], txo.from == ^from_address and txo.to == ^to_address)
+    where(query, [_ev, txo], txo.from == ^from_address and txo.to == ^to_address)
   end
 
   defp filter_address(query, %{"activity" => address}) do
     address = Address.from_text(address)
-    where(query, [txo], txo.from == ^address or txo.to == ^address)
+    where(query, [_ev, txo], txo.from == ^address or txo.to == ^address)
   end
 
   defp filter_address(query, _params), do: query
 
   defp filter_token(query, %{"token" => token}) do
-    where(query, [txo], txo.token == ^token)
+    where(query, [_ev, txo], txo.token == ^token)
   end
 
   defp filter_token(query, _params), do: query
 
   defp filter_reason(query, %{"reason" => reason}) do
-    where(query, [txo], txo.reason == ^reason)
+    where(query, [_ev, txo], txo.reason == ^reason)
   end
 
   defp filter_reason(query, _), do: query
 
   # defp filter_select(query, %{"fmt" => "list"}) do
-  #   select(query, [o, ev], [
+  #   select(query, [ev, o], [
   #     o.txid,
   #     o.from,
   #     o.to,
@@ -97,8 +97,8 @@ defmodule Ipncore.Txo do
 
   defp filter_select(query, _params) do
     query
-    |> join(:inner, [o], tk in Token, on: tk.id == o.token)
-    |> select([o, ev, tk], %{
+    |> join(:inner, [_ev, o], tk in Token, on: tk.id == o.token)
+    |> select([ev, o, tk], %{
       id: o.txid,
       from: o.from,
       to: o.to,
@@ -116,13 +116,13 @@ defmodule Ipncore.Txo do
         order_by(query, [txo, ev], asc: ev.block_index, asc: ev.time, desc: txo.txid, asc: txo.ix)
 
       "most_value" ->
-        order_by(query, [txo], desc: txo.value)
+        order_by(query, [_ev, txo], desc: txo.value)
 
       "less_value" ->
-        order_by(query, [txo], asc: txo.value)
+        order_by(query, [_ev, txo], asc: txo.value)
 
       _ ->
-        order_by(query, [txo, ev], desc: ev.block_index, desc: ev.time, asc: txo.txid, asc: txo.ix)
+        order_by(query, [ev, txo], desc: ev.block_index, desc: ev.time, asc: txo.txid, asc: txo.ix)
     end
   end
 

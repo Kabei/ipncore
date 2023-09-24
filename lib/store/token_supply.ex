b5d529f54837457003456d008af4a6b65d7e906b
kabei <kambei.sapote@gmail.com>
2023-09-24 18:50:19 -0400

    udpate

--- lib/store/token_supply.ex
+++ lib/store/token_supply.ex
@@ -8,21 +8,21 @@ defmodule TokenSupply do
   @spec get(token_id :: binary) :: integer()
   def get(token_id) do
     key = gen_key(token_id)
-    {_, supply} = DetsPlus.lookup(@table, key, 0)
+    {_, supply} = DetsPlus.lookup(@table, key, {nil, 0})
     supply
   end
 
   @spec add(token_id :: binary, amount :: integer()) :: :ok | {:error, term()}
   def add(token_id, amount) do
     key = gen_key(token_id)
-    {_, supply} = DetsPlus.lookup(@table, key, 0)
+    {_, supply} = DetsPlus.lookup(@table, key, {nil, 0})
     DetsPlus.insert(@table, {key, supply + amount})
   end
 
   @spec subtract(token_id :: binary, amount :: integer()) :: :ok | {:error, term()}
   def subtract(token_id, amount) do
     key = gen_key(token_id)
-    {_, supply} = DetsPlus.lookup(@table, key, 0)
+    {_, supply} = DetsPlus.lookup(@table, key, {nil, 0})
     DetsPlus.insert(@table, {key, supply - amount})
   end
 end

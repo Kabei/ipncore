fcbe6a8c6b7866b5985581a0bd01451f7d3a1f8b
kabei <kambei.sapote@gmail.com>
2023-10-21 13:52:43 -0400

    update

--- lib/funx/wallet_funx.ex
+++ lib/funx/wallet_funx.ex
@@ -1,22 +1,15 @@
 defmodule Ippan.Funx.Wallet do
   alias Ippan.Address
 
-  def subscribe(
-        %{wallets: {_wallet_dets, wallet_tx}},
-        pubkey,
-        validator_id,
-        sig_type
-      ) do
+  def subscribe(_, pubkey, validator_id, sig_type) do
     pubkey = Fast64.decode64(pubkey)
     id = Address.hash(sig_type, pubkey)
-
-    DetsPlux.put(wallet_tx, id, {pubkey, validator_id})
+    tx = DetsPlux.tx(:wallet)
+    DetsPlux.put(tx, id, {pubkey, validator_id})
   end
 
-  def unsubscribe(%{
-        id: account_id,
-        wallets: {_wallet_dets, wallet_tx}
-      }) do
-    DetsPlux.delete(wallet_tx, account_id)
+  def unsubscribe(%{id: account_id}) do
+    tx = DetsPlux.tx(:wallet)
+    DetsPlux.delete(tx, account_id)
   end
 end

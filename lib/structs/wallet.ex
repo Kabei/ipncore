f5f80ab0950a8294315fbd4ebe7a6eb7cd933c24
kabei <kambei.sapote@gmail.com>
2023-10-31 23:35:10 -0400

    update counter nonce

--- lib/structs/wallet.ex
+++ lib/structs/wallet.ex
@@ -45,37 +45,33 @@ defmodule Ippan.Wallet do
   end
 
   @spec update_nonce(DetsPlux.db(), DetsPlux.transaction(), binary, integer()) ::
-          non_neg_integer() | :error
+          true | :error
   def update_nonce(dets, tx, from, nonce) do
-    count = DetsPlux.get_tx(dets, tx, from, 0) + 1
+    DetsPlux.get_cache(dets, tx, from, 0)
 
-    if count == nonce do
-      DetsPlux.put(tx, from, count)
-      count
+    if DetsPlux.update_counter(tx, from, 1) == nonce do
+      true
     else
       :error
     end
   end
 
   @spec update_nonce!(DetsPlux.db(), DetsPlux.transaction(), binary, integer()) ::
-          non_neg_integer() | :error
+          nil | :error
   def update_nonce!(dets, tx, from, nonce) do
-    count = DetsPlux.get_tx(dets, tx, from, 0) + 1
+    DetsPlux.get_cache(dets, tx, from, 0)
 
-    if count == nonce do
-      DetsPlux.put(tx, from, count)
-      count
-    else
+    if DetsPlux.update_counter(tx, from, 1) != nonce do
       raise IppanError, "Invalid nonce"
     end
   end
 
   @spec gte_nonce!(DetsPlux.db(), DetsPlux.transaction(), binary, integer()) ::
-          binary() | no_return()
+          nil | no_return()
   def gte_nonce!(dets, tx, from, nonce) do
-    count = DetsPlux.get_tx(dets, tx, from, 0)
+    DetsPlux.get_cache(dets, tx, from, 0)
 
-    if nonce <= count do
+    if nonce <= DetsPlux.update_counter(tx, from, 1) do
       raise IppanError, "Invalid nonce"
     end
   end

45f3680cf6f7f41338837aad8e34b776dde9f4fc
kabei <kambei.sapote@gmail.com>
2023-11-08 08:53:28 -0400

    wallet.sub pay fees

--- lib/funx/wallet_funx.ex
+++ lib/funx/wallet_funx.ex
@@ -1,17 +1,31 @@
 defmodule Ippan.Funx.Wallet do
-  alias Ippan.Address
+  require BalanceStore
+  alias Ippan.{Address, Utils}
 
   def new(_, pubkey, validator_id, sig_type) do
     pubkey = Fast64.decode64(pubkey)
     id = Address.hash(sig_type, pubkey)
     tx = DetsPlux.tx(:wallet)
-    DetsPlux.put(tx, {id, pubkey, validator_id})
+    DetsPlux.put(tx, {id, pubkey, validator_id, sig_type})
   end
 
-  def subscribe(%{id: account_id}, validator_id) do
-    dets = DetsPlux.get(:wallet)
-    tx = DetsPlux.tx(:wallet)
-    DetsPlux.get_cache(dets, tx, account_id)
-    DetsPlux.update_element(tx, account_id, 2, validator_id)
+  def subscribe(
+        %{id: from, validator: %{fa: fa, fb: fb, owner: vOwner}, size: size},
+        validator_id
+      ) do
+    fees = Utils.calc_fees(fa, fb, size)
+    dets = DetsPlux.get(:balance)
+    tx = DetsPlux.tx(dets, :balance)
+
+    case BalanceStore.pay_fee(from, vOwner, fees) do
+      :error ->
+        :error
+
+      _ ->
+        wdets = DetsPlux.get(:wallet)
+        wtx = DetsPlux.tx(:wallet)
+        DetsPlux.get_cache(wdets, wtx, from)
+        DetsPlux.update_element(wtx, from, 2, validator_id)
+    end
   end
 end

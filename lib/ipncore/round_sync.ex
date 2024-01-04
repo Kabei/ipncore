f5f4a135fa73dbd673576db619ef6b93f153151d
kabei <kambei.sapote@gmail.com>
2024-01-03 21:23:29 -0400

    round sync

--- lib/ipncore/round_sync.ex
+++ lib/ipncore/round_sync.ex
@@ -183,6 +183,7 @@ defmodule RoundSync do
       case NetworkNodes.connect(node) do
         false ->
           Logger.warning("It is not possible connect to #{hostname}")
+          :idle
 
         _socket ->
           {:ok, round_id, node}

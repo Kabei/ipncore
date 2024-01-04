cb48665166a608cd862d98edfa057530f284fd62
kabei <kambei.sapote@gmail.com>
2024-01-04 16:06:14 -0400

    config

--- config/default.exs
+++ config/default.exs
@@ -67,36 +67,3 @@ config :ipncore, :http,
       reuseaddr: true
     ]
   ]
-
-# NTP servers
-config :ipncore, :ntp_servers, [
-  ~c"0.north-america.pool.ntp.org",
-  ~c"1.north-america.pool.ntp.org",
-  ~c"2.north-america.pool.ntp.org",
-  ~c"0.europe.pool.ntp.org",
-  ~c"1.europe.pool.ntp.org",
-  ~c"2.europe.pool.ntp.org",
-  ~c"0.asia.pool.ntp.org",
-  ~c"1.asia.pool.ntp.org",
-  ~c"2.asia.pool.ntp.org",
-  ~c"0.oceania.pool.ntp.org",
-  ~c"0.africa.pool.ntp.org",
-  ~c"hora.roa.es",
-  ~c"time.google.com",
-  ~c"time.cloudflare.com",
-  ~c"time.windows.com"
-]
-
-config :ipncore, :dns_resolve,
-  alt_nameservers: [
-    {{8, 8, 8, 8}, 53},
-    {{1, 0, 0, 1}, 53},
-    {{208, 67, 220, 220}, 53}
-  ],
-  nameservers: [
-    {{1, 1, 1, 1}, 53},
-    {{8, 8, 4, 4}, 53},
-    {{9, 9, 9, 9}, 53},
-    {{208, 67, 222, 222}, 53}
-  ],
-  timeout: 5_000

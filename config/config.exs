fb8b51088b51a54d3b7825e8071f37583f5830f0
kabei <kambei.sapote@gmail.com>
2024-01-16 14:04:01 -0400

    name

--- config/config.exs
+++ config/config.exs
@@ -14,14 +14,14 @@ else
 end
 
 # Blockchain setup
-config :ipncore, :name, "IPPAN"
+config :ipncore, :name, "IPPAN" <> if(prod, do: "", else: "-dev")
 config :ipncore, :token, System.get_env("NATIVE_TOKEN", "IPN")
 config :ipncore, :message_max_size, 8192
 config :ipncore, :version, 0
 config :ipncore, :max_block_size, 10_485_760
 config :ipncore, :max_block_data_size, 10_000_000
 config :ipncore, :round_timeout, :timer.seconds(40)
-config :ipncore, :block_extension, "blo"
+config :ipncore, :block_extension, "bk"
 config :ipncore, :decode_extension, "dec"
 config :ipncore, :note_max_size, 255
 config :ipncore, :max_tx_amount, 1_000_000_000_000_000

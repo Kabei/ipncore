2720972dfff6834630bba76196a8d86ea149ab07
kabei <kambei.sapote@gmail.com>
2024-03-07 02:06:03 -0400

    synced wait conn

--- lib/p2p/cluster/cluster_server.ex
+++ lib/p2p/cluster/cluster_server.ex
@@ -10,19 +10,24 @@ defmodule Ippan.ClusterServer do
   def handle_connection(socket, state) do
     # Logger.debug("handle_connection #{inspect(state)}")
     tcp_socket = socket.socket
+    synced = :persistent_term.get(:status) == :synced
 
-    case P2P.server_handshake(
-           tcp_socket,
-           :persistent_term.get(:net_privkey),
-           &ClusterNodes.fetch/1
-         ) do
-      {:ok, id, %{sharedkey: sharedkey} = node, timeout} ->
-        ClusterNodes.on_connect(id, node, @via)
+    if synced do
+      case P2P.server_handshake(
+             tcp_socket,
+             :persistent_term.get(:net_privkey),
+             &ClusterNodes.fetch/1
+           ) do
+        {:ok, id, %{sharedkey: sharedkey} = node, timeout} ->
+          ClusterNodes.on_connect(id, node, @via)
 
-        {:continue, %{id: id, socket: tcp_socket, sharedkey: sharedkey}, timeout}
+          {:continue, %{id: id, socket: tcp_socket, sharedkey: sharedkey}, timeout}
 
-      _ ->
-        {:close, state}
+        _ ->
+          {:close, state}
+      end
+    else
+      {:close, state}
     end
   end
 
